074bbaeb20eb0a353dcc4d3307e0d8cafc2c3b78
ABHIJAT PATEL <abhijatpatelfaizabad@gmail.com>
2026-08-05 01:57:39 +0530

    Time: 8 ms (42.01%), Space: 13.2 MB (57.7%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
-    int value(char c){
-         if(c == 'I') return 1;
-         if(c == 'V') return 5;
-         if(c == 'X') return 10;
-         if(c == 'L') return 50;
-         if(c == 'C') return 100;
-         if(c == 'D') return 500;
-         return 1000;
-
-}
 int romanToInt(string s) {
         int ans = 0;
+        map<char,  int> mp = {{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
         
-        for(int i =0; i < s.length(); i++){
+        for(int i =0; i < s.size(); i++){
 
-            if(i+1 < s.length() && value(s[i]) < value(s[i+1]))
-            ans -= value(s[i]);
-
+            if(i+1 < s.size() && mp[s[i]] < mp[s[i+1]]){
+                ans -= mp[s[i]];
+        }
             else 
-                ans += value(s[i]);
-                }
-                return ans;
+            {
+                ans += mp[s[i]];
             }
+        }
+            return ans;
+        }
         };
