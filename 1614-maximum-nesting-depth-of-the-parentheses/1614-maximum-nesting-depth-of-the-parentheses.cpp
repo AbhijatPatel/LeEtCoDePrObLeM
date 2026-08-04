b2c64243e899d1e68fc52ed3290b4d586f913631
ABHIJAT PATEL <abhijatpatelfaizabad@gmail.com>
2026-08-05 01:15:27 +0530

    Time: 0 ms (100%), Space: 8.5 MB (24.75%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        stack<char>st;
+        int maxDepth = 0;
+
+        for(int i=0;i<s.size();i++){
+            if(s[i] == ('(')){
+                st.push(s[i]);
+
+                maxDepth = max(maxDepth,(int)st.size());
+            }
+            else if(s[i] == (')')){
+                st.pop();
+            }
+        }
+        return maxDepth;
+    }
+};
