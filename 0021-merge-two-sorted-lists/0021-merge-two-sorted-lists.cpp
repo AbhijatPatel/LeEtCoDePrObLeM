5d08d5fb87de14d43666b8f7160a995c35ab1c7c
ABHIJAT PATEL <abhijatpatelfaizabad@gmail.com>
2026-08-13 16:38:02 +0530

    Time: 0 ms (100%), Space: 19.3 MB (88.43%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -12,45 +12,63 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
 
-        if (list1 == NULL)
-            return list2;
+        // if (list1 == NULL)
+        //     return list2;
 
-        if (list2 == NULL)
-            return list1;
+        // if (list2 == NULL)
+        //     return list1;
 
-        ListNode* head;
+        // ListNode* head;
 
-        if (list1->val <= list2->val) {
-            head = list1;
-            list1 = list1->next;
-        }
-        else {
-            head = list2;
-            list2 = list2->next;
-        }
+        // if (list1->val <= list2->val) {
+        //     head = list1;
+        //     list1 = list1->next;
+        // }
+        // else {
+        //     head = list2;
+        //     list2 = list2->next;
+        // }
 
-        ListNode* tail = head;
+        // ListNode* tail = head;
 
-        while (list1 != NULL && list2 != NULL) {
+        // while (list1 != NULL && list2 != NULL) {
 
-            if (list1->val <= list2->val) {
-                tail->next = list1;
-                list1 = list1->next;
+        //     if (list1->val <= list2->val) {
+        //         tail->next = list1;
+        //         list1 = list1->next;
+        //     }
+        //     else {
+        //         tail->next = list2;
+        //         list2 = list2->next;
+        //     }
+
+        //     tail = tail->next;
+        // }
+
+        // if (list1 != NULL)
+        //     tail->next = list1;
+        // else
+        //     tail->next = list2;
+
+        // return head;
+        ListNode* t1 = list1;
+        ListNode* t2 = list2;
+        ListNode* dummyNode = new ListNode(-1); 
+        ListNode*  temp =  dummyNode;
+        while(t1 != NULL && t2 != NULL){
+            if(t1->val < t2->val){
+                temp->next = t1;
+                temp = t1;
+                t1 = t1->next;
             }
-            else {
-                tail->next = list2;
-                list2 = list2->next;
+            else{
+                temp->next = t2;
+                temp = t2;
+                t2 =t2->next;
             }
-
-            tail = tail->next;
         }
-
-        if (list1 != NULL)
-            tail->next = list1;
-        else
-            tail->next = list2;
-
-        return head;
-        
+        if(t1) temp->next = t1;
+        else temp->next = t2;
+        return dummyNode->next;
     }
 };
