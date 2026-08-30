5e863b3441151409f2a16f79e7d4c9aae4c1f38d
ABHIJAT PATEL <abhijatpatelfaizabad@gmail.com>
2026-08-31 04:19:12 +0530

    Time: 0 ms (100%), Space: 21.5 MB (97.29%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        
+        vector<int> st;
+
+        for (int i = 0; i < asteroids.size(); i++) {
+
+            int current = asteroids[i];
+            bool destroyed = false;
+
+            while (!st.empty() && st.back() > 0 && current < 0) {
+
+                if (st.back() < -current) {
+                    st.pop_back();
+                }
+                else if (st.back() == -current) {
+                    st.pop_back();
+                    destroyed = true;
+                    break;
+                }
+                else {
+                    destroyed = true;
+                    break;
+                }
+            }
+
+            if (!destroyed) {
+                st.push_back(current);
+            }
+        }
+
+        return st;
+    }
+};
