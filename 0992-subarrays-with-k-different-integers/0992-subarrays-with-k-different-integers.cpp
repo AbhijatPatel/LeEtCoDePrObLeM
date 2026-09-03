3d7081b5d51b452a2bb096ac9d5c1edf0fb4668d
ABHIJAT PATEL <abhijatpatelfaizabad@gmail.com>
2026-09-03 05:47:38 +0530

    Time: 64 ms (82.37%), Space: 55.3 MB (75.54%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
-   int atMostK(vector<int> & nums,int k){
-    if(k<0)
-    return 0;
-
-    map<int,int> mp;
-    int l=0;
-    int cnt =0;
-
-    for(int r=0;r<nums.size();r++){
-        mp[nums[r]]++;
-
-        while(mp.size()>k){
-            mp[nums[l]]--;
-
-            if(mp[nums[l]]==0)
-            mp.erase(nums[l]);
-            l++;
-        }
-        cnt += (r-l+1);
+int atMost(vector<int> & nums,int k){
+    if(k == 0){
+        return 0;
     }
-    return cnt;
+    int left = 0;
+    int ans = 0;
+
+    unordered_map<int, int> mp;
+
+    for (int right = 0; right < nums.size(); right++) {
+
+        mp[nums[right]]++;
+
+        while (mp.size() > k) {
+
+            mp[nums[left]]--;
+
+            if (mp[nums[left]] == 0) {
+                mp.erase(nums[left]);
+            }
+
+            left++;
+        }
+
+        ans += right - left + 1;
+    }
+
+    return ans;
 }
-int subarraysWithKDistinct(vector<int> & nums,int k){
-    return atMostK(nums,k)-atMostK(nums,k-1);
-        
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return atMost(nums,k)- atMost(nums,k-1);
     }
 };
